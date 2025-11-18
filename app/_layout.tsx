04e46b0b995f33fd3ccd9351aace4c3e9c8977ae
fersonull <culaspero@gmail.com>
2025-11-18 18:38:36 +0800

    refactor: remove header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="index" options={{
+          headerShown: false
+        }} />
+      </Stack>
+    </SafeAreaView>
+
+  );
 }
